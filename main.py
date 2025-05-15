49de6426180cd2177e5e3985f4cff7d206a6f4be
Adam Östberg <adamost03@gmail.com>
2025-05-15 14:46:46 +0200

    Adds ability to via user interface download market dataa and display mean closing value

--- main.py
+++ main.py
@@ -13,12 +13,36 @@ class MyApp:
         self.root.title("Market Data")
         self.root.geometry("1000x600")
 
-        self.label = ttk.Label(root, text="Welcome")
+        self.label = ttk.Label(self.root, text="Welcome")
         self.label.pack()
 
-    def create_wdigets(self):
-        
+        self.create_widgets()
 
+    def create_widgets(self):
+        # Add input for selecting symbol to get data from
+        self.symbol_label = ttk.Label(self.root, text="Symbol name: ")
+        self.symbol_label.pack(pady=10)
+
+        self.entry = ttk.Entry(self.root)
+        self.entry.pack()
+
+        self.get_data_button = ttk.Button(
+            self.root, 
+            text="Get data", 
+            command=lambda: self.get_symbol_data(self.entry.get())
+        )
+        self.get_data_button.pack(pady=10)
+
+        # change this later
+        self.mean_label = ttk.Label(text="No Data")
+        self.mean_label.pack()
+
+    def get_symbol_data(self, sym):
+        sym_data = yf.download(sym, period="1mo")
+        mean = np.mean(sym_data["Close"])
+        self.mean_label.config(text=f"Mean closing: {mean}")
+        
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
