3d099db54a62af4ec8fc03c4b1d4cc7a101f487b
adamostberg <adamost03@gmail.com>
2025-05-17 12:12:44 +0200

    Adds ability to overlay a SMA analysis with a custom window

--- main.py
+++ main.py
@@ -22,26 +22,77 @@ class MyApp:
         # This is a placeholder for the plot canvas
         self.canvas = None
 
+        # Default ticker options
+        self.sym = "AAPL"
+        self.period = "3mo"
+        self.interval = "1d"
+        self.ta = []
+
         self.create_widgets()
 
     def create_widgets(self):
+        # Container frame for all the options
+        self.container = ttk.Frame(self.root)
+        self.container.pack(padx=10, pady=10, fill="x")
+
+        # Left box, for ticker options
+        self.t_box = ttk.LabelFrame(self.container, text="Ticker Options")
+        self.t_box.pack(side="left", expand=True, fill="both", padx=10)
+        
         # Add input for selecting symbol to get data from
-        self.symbol_label = ttk.Label(self.root, text="Symbol name: ")
+        self.symbol_label = ttk.Label(self.t_box, text="Symbol name: ")
         self.symbol_label.pack(pady=10)
 
-        self.entry = ttk.Entry(self.root)
+        self.entry = ttk.Entry(self.t_box, background="white")
         self.entry.pack()
+
+        # Right box, for ta options
+        self.a_box = ttk.LabelFrame(self.container, text="Technical Analysis")
+        self.a_box.pack(side="left", expand=True, fill="both", padx=10)
+
+        self.create_sma_options(self.a_box)
 
         # Button used for getting and plotting a symbol
         self.get_data_button = ttk.Button(
             self.root, 
             text="Get data", 
-            command=lambda: self.get_symbol_data(self.entry.get())
+            command=lambda: self.new_sym(self.entry.get())
         )
         self.get_data_button.pack(pady=10)
 
-    def get_symbol_data(self, sym):
-        sym_data = yf.download(sym, period="3mo", interval="1d")
+        #self.create_plot()
+
+    def create_sma_options(self, box):
+        self.sma_box = ttk.LabelFrame(box, text="SMA Analysis", style="White.TLabel")
+        self.sma_box.pack(side="left", expand=True, fill="both", padx=10)
+
+        self.win_label = ttk.Label(self.sma_box, text="Window: ")
+        self.win_label.pack(side="left")
+
+        self.win_entry = ttk.Entry(self.sma_box, background="white")
+        self.win_entry.pack(side="left")
+
+        self.sma_button = ttk.Button(
+            self.sma_box, 
+            text="Add SMA", 
+            command=lambda: self.add_sma_analysis(int(self.win_entry.get()))
+        )
+        self.sma_button.pack(side="left")
+
+    def add_sma_analysis(self, window):
+        # Adds a SMA analysis to the ta list and updates plot
+        sma = self.data["Close"].rolling(window=window).mean()
+        self.ta.append(("SMA", sma))
+        self.create_plot()
+
+    def new_sym(self, sym):
+        self.sym = sym
+        self.ta = []
+        self.create_plot()
+
+    def create_plot(self):
+        self.data = yf.download(self.sym, period=self.period, interval=self.interval)
+        sym_data = self.data
         sym_data.index.name = "Date"
 
         # Clean data for mpfinance, we need single index not multi
@@ -52,12 +103,37 @@ class MyApp:
         if self.canvas:
             self.canvas.get_tk_widget().destroy()
 
+        additional_plots = []
+
+        # Plot TA options
+        for option in self.ta:
+            if option[0] == "SMA":
+                additional_plots.append(mpf.make_addplot(option[1]))
+
         # Create the plot
-        fig, ax = plt.subplots(figsize=(7,5))
-        mpf.plot(sym_data, type="candle", style="yahoo", ax=ax)
-        ax.set_title(f"{sym.upper()} over 1 month")
-        ax.set_xlabel("Date")
-        ax.set_ylabel("Price (USD)")
+        fig, ax = None, None
+
+        if additional_plots:
+            fig, ax = mpf.plot(
+                sym_data,
+                type="candle",
+                style="yahoo",
+                addplot=additional_plots,
+                returnfig=True,
+                figsize=(7,5),
+                title=f"{self.sym.upper()} over {self.period} (interval = {self.interval})",
+                ylabel="Price (USD)",
+            )
+        else:
+            fig, ax = mpf.plot(
+                sym_data,
+                type="candle",
+                style="yahoo",
+                returnfig=True,
+                figsize=(7,5),
+                title=f"{self.sym.upper()} over {self.period} (interval = {self.interval})",
+                ylabel="Price (USD)",
+            )
 
         # Embed the plot in tkinter
         self.canvas = FigureCanvasTkAgg(fig, master=self.root)
@@ -65,7 +141,6 @@ class MyApp:
         self.canvas.get_tk_widget().pack(pady=10)
         
         
-
 if __name__ == "__main__":
     root = tk.Tk()
     app = MyApp(root)
